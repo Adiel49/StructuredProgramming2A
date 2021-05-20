5aefba1b8a0143e14027d0717e4c9ee5db549e8c
Adiel <adivil49@gmail.com>
2021-05-19 21:39:02 -0500

    act4

---
+++ unit1/activity_04.py
@@ -0,0 +1,11 @@
+
+num1=0
+num2=0
+add=0 
+m=0
+num1=int(input("input number 1:"))
+num2=int(input("input number 2:"))
+add=(num1+num2)
+m=(num1*num2)
+print("result m: ", m) 
+print("result add:" , add)
