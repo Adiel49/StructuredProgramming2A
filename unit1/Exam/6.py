4e76d85b80fcb4bc85ef5ce496b7f5bb8c6fdc80
adiel49 <adivil49@gmail.com>
2021-05-27 22:09:02 -0500

    In process

---
+++ unit1/Exam/6.py
@@ -0,0 +1,12 @@
+
+l=0 
+w=0 
+a=0
+l=int(input("Input length: "))
+w=int(input("Input Width: "))
+a=l*w
+if (w==l):
+    print("The figure is square")
+    print("Area is: ", a)
+else: 
+    print("Area is: ", a)
