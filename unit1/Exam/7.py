c72bba44028f140c1ca8174d7cb4e0da9c5d4160
adiel49 <adivil49@gmail.com>
2021-05-27 22:43:40 -0500

    In process

---
+++ unit1/Exam/7.py
@@ -0,0 +1,16 @@
+
+a=0
+b=0
+c=0
+a=int(input("Input number A: "))
+b=int(input("Input number B:"))
+c=int(input("Input number C: "))
+if(a>b):
+    if(a>c):
+       print("A is the greatest number")
+else:
+    if(b>c):
+       print("B is the greatest number")
+    else:
+       print("C is the greatest number")
+
