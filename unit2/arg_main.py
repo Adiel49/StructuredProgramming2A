5ab8ce4ca21cfcc1f276deae1ed068b3d1526b43
adiel49 <adivil49@gmail.com>
2021-05-27 21:34:44 -0500

    In Process

---
+++ unit2/arg_main.py
@@ -0,0 +1,18 @@
+import sys
+
+#Variable Globales##
+my_int=0
+my_str=""
+my_float=0.0
+my_bool=True ##false
+my_list =[] #list
+
+if __name__=="__main__":
+        my_int=12
+        my_str="Hola mundo"
+        my_float=3.14
+        my_bool=False ##False
+        my_list=[1,2,3,4,5] ##list()
+        ## 0,1,2,3,4
+        print(my_list, my_str,my_float, my_bool, my_list[])
+        #print(sys.argv)
