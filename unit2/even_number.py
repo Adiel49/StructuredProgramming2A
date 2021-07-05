b9ed2ed6c272bcbce3f6981d1b726c49b80b998c
Adiel49 <adivil49@gmail.com>
2021-07-04 23:12:24 -0500

    In Process

---
+++ unit2/even_number.py
@@ -0,0 +1,24 @@
+a=int(input("Input a number: "))
+
+if a%2==0:
+    r="and is Even"
+else:
+    r="and is odd"
+
+
+def ep(a):
+    con=int(0)
+    r=True
+    for i in range(1,a+1):
+        if (a%i==0):
+            con+=1
+        if (con>2):
+            r=False
+            break
+    return r
+if (ep(a)==True):
+    print("The number",a,"is prime",r)
+    ##print("",r)
+else:
+    print("The number",a,"isn't prime",r)
+    ##print("",r)
