77a8f2d67f3f75d69fd29019dbd40a25b0397651
Adiel49 <adivil49@gmail.com>
2021-07-05 23:28:47 -0500

    In process

---
+++ unit2/dic.py
@@ -0,0 +1,8 @@
+import sys
+
+
+def say_hello_to(any_name):
+    print("hello", any_name, "!")
+
+    if __name__ == "__main__":
+        say_hello_to("Maria")
---
+++ unit2/function_intro.py
@@ -0,0 +1,12 @@
+from sys import argv as ag
+
+
+def add_to_number(num1, num2):
+    print('StartProgram: addToNumbers executed...\n')
+    return num1 + num2
+    return result
+
+
+if __name__ == "__main__":
+    print(
+        f'La suma de dos numeros = {add_to_number( int(ag[1]), int(ag[2]) )}')
